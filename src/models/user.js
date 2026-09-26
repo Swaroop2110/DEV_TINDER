6b502ca2e5ab0680ffeaefb8bc7a8a1f0f6f2a5a
Swaroop2110 <swaroop2110@gmail.com>
2026-09-26 22:30:47 +0530

    validators

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -16,10 +16,20 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address");
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Password is not strong enough");
+            }
+        }
     },
     age: {
         type: Number,
@@ -36,6 +46,11 @@ const userSchema = new mongoose.Schema({
     photoUrl:{
         type: String,
         default:"https://geographyandyou.com/images/user-profile.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL");
+            }   
+        }
     },
     about:{
         type: String,
